004223a628b5f58ad33f76b6a564ab59fcd02690
Yurii Torbyk <yurii@findcanary.com>
2022-10-07 21:29:03 -0500

    Publish customized v0.12.0.10 version

--- Formula/warden.rb
+++ Formula/warden.rb
@@ -2,10 +2,10 @@
 class Warden < Formula
   desc "Warden is a CLI utility for working with docker-compose environments"
   homepage "https://warden.dev"
-  version "0.12.0.9"
-  url "https://github.com/ytorbyk/warden/archive/release-0.12.0.9.tar.gz"
-  sha256 "4e49cc3dccf93faf940711502080b5e88455f4361ea7596944175f8e21509f9f"
-  head "https://github.com/ytorbyk/warden.git", :branch => "release-0.12.0.9"
+  version "0.12.0.10"
+  url "https://github.com/ytorbyk/warden/archive/release-0.12.0.10.tar.gz"
+  sha256 "5b32973a871764830587f9e958d48fb49e0b31ed2fc808802b474d4b15521219"
+  head "https://github.com/ytorbyk/warden.git", :branch => "release-0.12.0.10"
 
   def install
     prefix.install Dir["*"]
